5f9d47b0b4f1091ec290228166545e91d5918b92
Ouko Ezra <oukoezra@Oukos-MacBook-Pro.local>
2023-04-27 10:38:05 +0300

    add modal component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Modal },
+  data() {
+    return {
+      title: 'vue app'
+    }
+  },
+  
+  
 }
 </script>
 
